f43c7174ca6bc3d0339078c70c1056aa8d14eb63
dajospin <l2211012830@ogr.sdu.edu.tr>
2025-11-27 14:55:07 +0300

    first commit

---
+++ app_api/models/db.js
@@ -0,0 +1,23 @@
+var mongoose = require( 'mongoose' );
+var dbURI = 'mongodb://localhost/mekanbul';
+mongoose.connect(dbURI);
+mongoose.connection.on("connected",function(){
+    console.log(dbURI+ "adresindaki  veritabani   baglandi\n")
+});
+mongoose.connection.on("error",function(){
+    console.log( "baglanti hatasi\n ")
+});
+
+mongoose.connection.on("disconnected",function(){
+    console.log(dbURI+ " baglanti\n")
+});
+
+//uygulama kapadiginda kapat
+
+process.on("SIGINT",function(){
+    mongoose.connection.close();
+    console.log("Baglanti kapatildi");
+    process.exit(0);
+});
+
+require("./venue"); 
---
+++ app_api/models/venue.js
@@ -0,0 +1,33 @@
+var mongoose = require( "mongoose" );
+
+ var hour= new mongoose.Schema({
+    days:{type: String, required:true},
+    open: String,
+    close: String,
+    isClosed:{type:Boolean, required:false}
+ });
+
+  var comment = new mongoose.Schema({
+    author:{type: String, required : true},
+    rating:{type:Number, default: 0,min :0,max:5},
+    text: {type: String, required: true},
+    date: {type: Date, default:Date.now}
+
+
+
+  }) ;
+
+
+var venue = new mongoose.Schema({
+    name:{type:String, required: true},
+    adress:{type:String, required: true},
+    rating:{type:Number, default: 0,min :0,max:5},
+    foodanddrink:[String],
+    distance :{type:Number},
+    coordinates:{type:Number,index:'2dsphere'},
+    hours:[hour],
+    comments:[comment]
+
+  }); 
+
+  mongoose.model("venue", venue,"venues");
